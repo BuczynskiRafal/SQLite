1090513423368d3d94ce672cb06361c9d9eff1ff
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-10-04 14:20:41 +0200

    SELECT

--- 01. DQL/01_Commands.sql
+++ 01. DQL/01_Commands.sql
@@ -1,9 +1,22 @@
--- Proste zapytanie SELECT
-SELECT 20*10;
-/*
-Komentarz 
-wielowierszowy
-*/
+-- Polecenie SELECT.
+SELECT 20 * 5;
+SELECT 20 - 1;
+SELECT 20 + 5;
+SELECT 20 / 5;
+SELECT 21 / 5;
+-- Działania na liczbach rzeczywistych, odpowiednik liczny float.
+SELECT 21 / 5.0;
+-- Dzielenie modulo
+SELECT 20 % 5;
+SELECT 21 % 5;
+-- wyraezenia tekstowe
+SELECT 'Python';
+SELECT 'Python', 'Java';
+--Łączenie tekstów (||)
+SELECT 'Python' || ' '|| '3.8.0';
+--Wyrażenia bool
+SELECT Treu, False;
+SELECT NULL;
 
--- Wydrukuj wersję SQLite
-SELECT sqlite_version();
+SELECT (100.0/95.0)-1 AS Daily_return,
+        'GPW' AS 'Market'
